55df2af2d784bfe8b889380cc8c518c30e4794f8
VitorFidelis <vitor.fidelis5587@gmail.com>
2025-09-02 13:56:46 -0300

    feat(usuarios/tipousuarios): criação das entidade de dominio usuario e tipo de usuario

---
+++ gateway/src/main/java/gateway/br/com/gateway/domain/model/TipoUsuario.java
@@ -0,0 +1,51 @@
+package gateway.br.com.gateway.domain.model;
+
+public class TipoUsuario {
+    private Long id;
+    private TipoUsuarioEnum nome;
+    private String descricao;
+    private Boolean ativo;
+
+    public TipoUsuario() {
+
+    }
+
+    public TipoUsuario(Long id, TipoUsuarioEnum nome, String descricao, Boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.ativo = ativo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TipoUsuarioEnum getNome() {
+        return nome;
+    }
+
+    public void setNome(TipoUsuarioEnum nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
