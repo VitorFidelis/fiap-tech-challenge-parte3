bf4260af2e34d7d0692ca3f0194fd33a3a451638
VitorFidelis <vitor.fidelis5587@gmail.com>
2025-09-02 22:09:52 -0300

    feat(UserMapper): Criação da classe UserMapper utilizando MapStruct, para realizar a conversão de entidade jpa para entidade de dominio e entidade de dominio para entidade jpa, dependencia adicionada no pom.xml.

--- gateway/src/main/java/gateway/br/com/gateway/infrastructure/persistence/mapper/UsuarioMapper.java
+++ gateway/src/main/java/gateway/br/com/gateway/infrastructure/persistence/mapper/UsuarioMapper.java
@@ -1,4 +1,15 @@
 package gateway.br.com.gateway.infrastructure.persistence.mapper;
 
-public class UsuarioMapper {
+import gateway.br.com.gateway.domain.model.Usuario;
+import gateway.br.com.gateway.infrastructure.persistence.entity.UsuarioEntity;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper
+public interface UsuarioMapper {
+    UsuarioEntity fromEntityJpa(Usuario usuario);
+    Usuario fromEntityDomain(UsuarioEntity usuarioEntity);
+    Page<Usuario> fromUsuarioPage(List<UsuarioEntity> usuarioEntityList);
 }
