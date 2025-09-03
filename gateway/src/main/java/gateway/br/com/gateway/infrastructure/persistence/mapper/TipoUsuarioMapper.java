88e3b4112a5af51963684ebcba14d2bbdb76687d
VitorFidelis <vitor.fidelis5587@gmail.com>
2025-09-03 16:45:15 -0300

    feat(TipoUsuarios): Criação da interface TipoUsuarioMapper para conversão de entidade jpa para entidade de dominio, e de entidade de dominio para entidade jpa utilizando MapStruct;

--- gateway/src/main/java/gateway/br/com/gateway/infrastructure/persistence/mapper/TipoUsuarioMapper.java
+++ gateway/src/main/java/gateway/br/com/gateway/infrastructure/persistence/mapper/TipoUsuarioMapper.java
@@ -1,4 +1,15 @@
 package gateway.br.com.gateway.infrastructure.persistence.mapper;
 
+import gateway.br.com.gateway.domain.model.TipoUsuario;
+import gateway.br.com.gateway.infrastructure.persistence.entity.TipoUsuarioEntity;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper
 public interface TipoUsuarioMapper {
+    TipoUsuarioEntity fromTipoUsuarioJpa(TipoUsuario tipoUsuario);
+    TipoUsuario fromTipoUsuarioDomain(TipoUsuarioEntity tipoUsuarioEntity);
+    Page<TipoUsuario> fromTipoUsuarioDomainPage(List<TipoUsuarioEntity> tipoUsuarioEntityList);
 }
